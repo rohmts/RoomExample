f5caf8aa79038c22bc8307544e89663f8978f1ba
rohmts <rohmatsuseno@gmail.com>
2018-05-05 13:48:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rohmts/roomexample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rohmts.roomexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
