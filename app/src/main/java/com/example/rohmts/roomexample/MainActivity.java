fe5e34ce3dac08abda9aa718083af17982d8c0f5
rohmts <rohmatsuseno@gmail.com>
2018-05-06 18:29:59 +0700

    Add user function

--- app/src/main/java/com/example/rohmts/roomexample/MainActivity.java
+++ app/src/main/java/com/example/rohmts/roomexample/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.rohmts.roomexample;
 
+import android.arch.persistence.room.Room;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static FragmentManager fragmentManager;
+    public static AppDatabase appDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+        appDatabase = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "userdb")
+                .allowMainThreadQueries().build();
+
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            fragmentManager.beginTransaction().add(R.id.fragment_container, new HomeFragment()).commit();
+        }
     }
 }
